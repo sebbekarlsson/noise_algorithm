352d13dec566f31dc1eafb90f790d1248a3f3281
= <sebbekarlsson97@gmail.com>
2015-11-03 23:08:13 +0100

    colors

--- __main__.py
+++ __main__.py
@@ -4,13 +4,13 @@ from math import *
 
 
 # This function puts data on each pixel, (we need something to start with)
-def precompile(image, data):
-    pixels = image.load()
-
+def precompile(image, pixels, amount):
     for x in range(0, image.width):
             for y in range(0, image.height):
-                value = int(data)
-                pixels[x, y] = (value, value, value)
+                r = int(255 / random.randint(1, amount))
+                g = int(255 / random.randint(1, amount))
+                b = int(255 / random.randint(1, amount))
+                pixels[x, y] = (r, g, b)
 
     return pixels
 
@@ -18,7 +18,7 @@ def precompile(image, data):
 def main():
 
     # How many times should we compile?
-    compiles = 5
+    compiles = 100
 
     # Width and height of image
     width = 640
@@ -26,12 +26,15 @@ def main():
 
     # Let's create the image
     image = Image.new('RGB', (width, height))
+    pixels = image.load()
     
     # Let's pre-compile the image. (putting data on each pixel so that we have something to start with)
-    pixels = precompile(image, compiles) 
+    pixels = precompile(image, pixels, 6)
 
     # Compile
     for i in range(0, compiles):
+
+        print('{p}%\r'.format(p=(i / compiles) * 100 ), end='\r')
 
         # Loop through each pixel
         for x in range(0, width):
@@ -45,19 +48,38 @@ def main():
                 current_pixel = pixels[x, y]
 
                 # Lets calculate the medium_value of each pixel
-                medium_value = ((left_pixel[0] + right_pixel[0] + top_pixel[0] + bottom_pixel[0]) / 4)
-                
-                # Calculating our final value and adding a random value to it
-                value = int(medium_value + random.randint(0, 24))
+                medium_r =\
+                (
+                    int((left_pixel[0])) +
+                    int((right_pixel[0])) +
+                    int((top_pixel[0])) +
+                    int((bottom_pixel[0]))
+                ) / 4
+
+                medium_g =\
+                (
+                    int((left_pixel[1])) +
+                    int((right_pixel[1])) +
+                    int((top_pixel[1])) +
+                    int((bottom_pixel[1]))
+                ) / 4
+
+                medium_b =\
+                (
+                    int((left_pixel[2])) +
+                    int((right_pixel[2])) +
+                    int((top_pixel[2])) +
+                    int((bottom_pixel[2]))
+                ) / 4
 
                 # Setting the values of the current pixel
-                current_pixel = (value, value , value)
+                current_pixel = (int(medium_r), int(medium_g) , int(medium_b))
                 
                 # Saving changes to current pixel
                 pixels[x, y] = current_pixel
 
-    # Saving image
-    image.save("result.png")
+        # Save image
+        image.save("results/result-{v}.png".format(v=i))
 
 
 if __name__ == '__main__':
